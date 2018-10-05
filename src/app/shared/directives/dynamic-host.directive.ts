0625b6f3d65a8ea3edcdec8b64627fd4f07df35e
Yarden <yardenron4@gmail.com>
2018-10-05 10:13:49 +0300

    Adding the host directive

---
+++ src/app/shared/directives/dynamic-host.directive.ts
@@ -0,0 +1,9 @@
+import { Directive, ViewContainerRef } from '@angular/core';
+
+@Directive({
+    selector: '[dynamic-host]'
+})
+export class DynamicHostDirective {
+
+    constructor(public viewContainerRef: ViewContainerRef) {}
+}
